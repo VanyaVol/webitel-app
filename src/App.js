c89cf4e0c0b702f2fa4ad401dc7875fd5c094172
VanyaVol <vanya.streetworkout.sw.vol@gmail.com>
2024-01-22 22:53:17 +0200

    First create

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Layout/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
